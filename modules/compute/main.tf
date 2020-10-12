587b815dd73f519487e9ab7e058871a4f6c93c2f
erskaggs <ericskaggs@gmail.com>
2020-10-11 19:06:09 -0500

    fixed compute module

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,22 +1,22 @@
 resource "google_compute_disk" "public_disk1" {
-  name = var.public_disk_name
-  type = var.public_disk_type
+  name    = var.public_disk_name
+  type    = var.public_disk_type
   project = var.project_id
-  size = var.public_disk_size
+  size    = var.public_disk_size
 }
 resource "google_compute_disk" "private_disk1" {
-  name = var.private_disk_name
-  type = var.private_disk_type
+  name    = var.private_disk_name
+  type    = var.private_disk_type
   project = var.project_id
-  size = var.private_disk_size
+  size    = var.private_disk_size
 }
 
 
 resource "google_compute_instance" "public_instance" {
-  name = var.public_compute_name
+  name         = var.public_compute_name
   machine_type = var.machine_type
-  zone = var."${module.vpc.}"
-  tags = ["ssh","http"]
+  zone         = "${module.vpc.subnetwork-1.region}-a"
+  tags         = ["ssh", "http"]
 
   boot_disk {
     initialize_params {
@@ -25,18 +25,17 @@ resource "google_compute_instance" "public_instance" {
   }
 
   network_interface {
-    network = module.vpc.vpc_network.name
+    network    = module.vpc.vpc_network.name
     subnetwork = module.vpc.subnetwork-1.name
 
-  access_config { }
+    access_config {}
   }
 }
-
 resource "google_compute_instance" "private_instance" {
-  name = var.private_compute_name
+  name         = var.private_compute_name
   machine_type = var.private_machine_type
-  zone = var.region"-a"
-  tags = ["ssh","http"]
+  zone         = "${module.vpc.subnetwork-3.region}-a"
+  tags         = ["ssh", "http"]
 
   boot_disk {
     initialize_params {
@@ -46,7 +45,7 @@ resource "google_compute_instance" "private_instance" {
 
   metadata_startup_script = "sudo dnf update; sudo dnf install -y apache2; sudo systemctl enable httpd; sudo systemctl start httpd"
   network_interface {
-    network = module.vpc.vpc_network.name
+    network    = module.vpc.vpc_network.name
     subnetwork = module.vpc.subnetwork-3.name
 
   }
